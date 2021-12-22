024c2297a825025ba496a162dcb1c688492449eb
master23456789 <91898768+master23456789@users.noreply.github.com>
2021-12-22 14:01:26 +0530

    Add files via upload

---
+++ byr.py
@@ -0,0 +1,24 @@
+#WAP to enter 2 numbers from the user and check whether the 1st no. is divisible by 3 or not while check the other no. which is divisible by 8 or not.
+k = int(input('The 1st number by the user: '))
+if k % 3 == 0:
+    print('The number is divisible by 3.')   
+elif k % 8 == 0:
+    print('The number is divisible by 8.')
+    
+r = int(input('The second number by the user: ')) 
+if r % 3 == 0:
+    print('The number is divisible by 3.')
+elif r % 8 == 0:
+    print('The number is divisible by 8.')
+  
+ 
+    
+       
+    
+
+
+
+
+
+
+
---
+++ pythons.py
@@ -0,0 +1,12 @@
+p = int(input('The number by the user: '))
+q = input('The str from the user: ')
+if p % 8 == 0:
+  print('The number is divisible by 8')
+else:
+    print('The number is not divisible by 8.');
+
+
+print('The elements of str from six to nine are',q[5:9])
+
+
+
